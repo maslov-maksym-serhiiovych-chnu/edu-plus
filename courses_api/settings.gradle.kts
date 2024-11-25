7c8154e3ac379e7f9d30d2c0bf8812a66f49132d
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-11-25 09:08:01 +0200

    initialize courses_api

---
+++ courses_api/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "courses_api"
---
+++ courses_api/src/main/java/ua/edu/chnu/courses_api/CoursesApiApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.chnu.courses_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApiApplication.class, args);
+	}
+
+}
