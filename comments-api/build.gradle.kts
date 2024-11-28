5d44e3f730fe22895b3b5c411e993889a517c01f
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-11-28 03:45:22 +0200

    comments-api: add OpenFeign dependency

--- comments-api/build.gradle.kts
+++ comments-api/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
 }
+val springCloudVersion by extra("2024.0.0-RC1")
 
 group = "ua.edu.chnu"
 version = "0.0.1-SNAPSHOT"
@@ -20,9 +21,15 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.withType<Test> {
