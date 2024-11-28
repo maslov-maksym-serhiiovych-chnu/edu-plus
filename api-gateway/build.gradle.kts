c0e5b8b70e5819799b2eb04bba78e8c7f87fa166
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-11-28 21:48:06 +0200

    api-gateway: initial commit

---
+++ api-gateway/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "ua.edu.chnu"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+extra["springCloudVersion"] = "2024.0.0-RC1"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
