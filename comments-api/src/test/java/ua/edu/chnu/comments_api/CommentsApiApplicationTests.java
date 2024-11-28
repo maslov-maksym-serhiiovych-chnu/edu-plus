200bb776283df84d92e5ed370ed9d3f401e6189e
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-11-28 02:47:20 +0200

    comments-api: initial commit

---
+++ comments-api/src/test/java/ua/edu/chnu/comments_api/CommentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.chnu.comments_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
